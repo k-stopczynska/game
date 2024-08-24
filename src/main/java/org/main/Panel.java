583f66d352105b1718b6b1fd72f9d2e0cbc24b62
k.stopczynska <k.stopczynska@gmail.com>
2024-08-24 11:03:57 +0200

    adding thread and drawing method

--- src/main/java/org/main/Panel.java
+++ src/main/java/org/main/Panel.java
@@ -1,12 +1,15 @@
 package org.main;
 
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 import javax.swing.plaf.DimensionUIResource;
 
-public class Panel extends JPanel {
-    
+
+public class Panel extends JPanel implements Runnable {
+
     final int ORIGINAL_TILE_SIZE = 16;
     final int SCALE = 3;
     final int TILE_SIZE = ORIGINAL_TILE_SIZE * SCALE;
@@ -15,9 +18,37 @@ public class Panel extends JPanel {
     final int SCREEN_WIDTH = TILE_SIZE * MAX_SCREEN_COL;
     final int SCREEN_HEIGHT = TILE_SIZE * MAX_SCREEN_ROW;
 
+    Thread thread;
+
     public Panel() {
         this.setPreferredSize(new DimensionUIResource(SCREEN_WIDTH, SCREEN_HEIGHT));
         this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true);
+    }
+
+    public void startThread() {
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    @Override
+    public void run() {
+        while (thread != null) {
+            update();
+            repaint();
+        }
+    }
+    
+    public void update() {
+        System.out.println("updating");
+    }
+
+    public void paintComponent(Graphics graphics) {
+        super.paintComponent(graphics);
+
+        Graphics2D graphics2D = (Graphics2D) graphics;
+        graphics2D.setColor(Color.PINK);
+        graphics2D.fillRect(100, 100, TILE_SIZE, TILE_SIZE);
+        graphics2D.dispose();
     }
 }
