ea30aefb13440b3ff7e0059d56e8d66868491e14
k.stopczynska <k.stopczynska@gmail.com>
2024-08-24 09:54:08 +0200

    initial commit

---
+++ src/main/java/org/main/Main.java
@@ -0,0 +1,15 @@
+package org.main;
+
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Adventure Game");
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+        }
+    }
